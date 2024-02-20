2deb6c914980e978839eb729b938fa2729329873
garyuu09 <ribereo.minami@gmail.com>
2024-02-20 20:08:24 +0900

    Initial Commit

---
+++ ComposableArchitectureTutorials/ComposableArchitectureTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureTutorialsApp.swift
+//  ComposableArchitectureTutorials
+//
+//  Created by Ryuga on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
