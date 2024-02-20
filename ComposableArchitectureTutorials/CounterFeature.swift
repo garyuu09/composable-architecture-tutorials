fdf4f7a961045c415a66ac1bf08db52f8f90eb96
garyuu09 <ribereo.minami@gmail.com>
2024-02-20 22:32:42 +0900

    Section 3 Managing a timer

--- ComposableArchitectureTutorials/CounterFeature.swift
+++ ComposableArchitectureTutorials/CounterFeature.swift
@@ -15,13 +15,19 @@ struct CounterFeature {
         var count = 0
         var fact: String?
         var isLoading = false
+        var isTimerRunning = false
     }
 
     enum Action {
         case decrementButtonTapped
         case factButtonTapped
+        case factResponse(String)
         case incrementButtonTapped
+        case timerTick
+        case toggleTimerButtonTapped
     }
+
+    enum CancelID { case timer }
 
     var body: some ReducerOf<Self> {
         Reduce { state, action in
@@ -35,19 +41,41 @@ struct CounterFeature {
                 state.fact = nil
                 state.isLoading = true
 
-                let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(state.count)")!)
-                // 🛑 'async' call in a function that does not support concurrency
-                // 🛑 Errors thrown from here are not handled
+                return .run { [count = state.count] send in
+                    let (data, _) = try await URLSession.shared.data(from: URL(string: "http://numbersapi.com/\(count)")!)
+                    let fact = String(decoding: data, as: UTF8.self)
+                    await send(.factResponse(fact))
+                }
 
-                state.fact = String(decoding: data, as: UTF8.self)
+            case let .factResponse(fact):
+                state.fact = fact
                 state.isLoading = false
-
                 return .none
 
             case .incrementButtonTapped:
                 state.count += 1
                 state.fact = nil
                 return .none
+
+            case .timerTick:
+              state.count += 1
+              state.fact = nil
+              return .none
+
+
+            case .toggleTimerButtonTapped:
+                state.isTimerRunning.toggle()
+                if state.isTimerRunning {
+                    return .run { send in
+                        while true {
+                            try await Task.sleep(for: .seconds(1))
+                            await send(.timerTick)
+                        }
+                    }
+                    .cancellable(id: CancelID.timer)
+                } else {
+                    return .cancel(id: CancelID.timer)
+                }
             }
         }
     }
@@ -80,6 +108,14 @@ struct CounterView: View {
                 .background(Color.black.opacity(0.1))
                 .cornerRadius(10)
             }
+            Button(store.isTimerRunning ? "Stop timer" : "Start timer") {
+              store.send(.toggleTimerButtonTapped)
+            }
+            .font(.largeTitle)
+            .padding()
+            .background(Color.black.opacity(0.1))
+            .cornerRadius(10)
+
             Button("Fact") {
                 store.send(.factButtonTapped)
             }
