b16d31cadc0373afe170f3a838b099aa1e5ced52
garyuu09 <ribereo.minami@gmail.com>
2024-02-20 21:15:08 +0900

    Section 3 Integrating into the app

--- ComposableArchitectureTutorials/ComposableArchitectureTutorialsApp.swift
+++ ComposableArchitectureTutorials/ComposableArchitectureTutorialsApp.swift
@@ -5,13 +5,18 @@
 //  Created by Ryuga on 2024/02/20.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct ComposableArchitectureTutorialsApp: App {
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store:ComposableArchitectureTutorialsApp.store)
         }
     }
 }
